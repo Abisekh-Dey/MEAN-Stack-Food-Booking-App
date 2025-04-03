58935a8838a85c7ec185ef4a9f030c09030e65b4
Abisekh Dey <167674834+Abisekh-Dey@users.noreply.github.com>
2025-04-03 19:37:09 +0530

    Update order_model.js

--- backend/models/order_model.js
+++ backend/models/order_model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
     user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    restaurant_id: { type: Schema.Types.ObjectId, ref: 'Restaurant', required: true },
+    // restaurant_id: { type: Schema.Types.ObjectId, ref: 'Restaurant', required: true },
     menu_items: [{
         menu_item_id: { type: Schema.Types.ObjectId, ref: 'MenuItem', required: true },
         quantity: { type: Number, required: true },
